1ff72de007c4c584ecae8ec6767091d7bb011c4f
JOSENILDO <JOSENILDO@Junior>
2018-01-13 09:51:22 -0200

    Iniciamos o Projeto

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        require_once 'config.php';
+
+        $sql = new Sql();
+        $usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
+        echo json_encode($usuarios);
+        ?>
+    </body>
+</html>
